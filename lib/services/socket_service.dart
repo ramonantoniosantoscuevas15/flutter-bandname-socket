2119d184d6b87fa1d502c072fc3b9099963f9031
ramonantoniosantoscuevas15 <82469695+ramonantoniosantoscuevas15@users.noreply.github.com>
2022-11-24 15:18:47 -0400

    cambios

--- lib/services/socket_service.dart
+++ lib/services/socket_service.dart
@@ -1,27 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:socket_io_client/socket_io_client.dart';
-
-enum ServerStatus { online, offline, conneting }
-
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+enum ServerStatus{
+  Online,
+  Offline,
+  Connecting,
+}
+ 
 class SocketService with ChangeNotifier {
-  final ServerStatus _serverStatus = ServerStatus.conneting;
-
-  SocketService() {
+  final ServerStatus _serverStatus = ServerStatus.Connecting;
+ 
+  SocketService(){
     _initConfig();
   }
-
-  void _initConfig() {
-    Socket socket = io(
-        'http://10.0.2.2:3000',
-        OptionBuilder()
-            .setTransports(['websocket']) // for Flutter or Dart VM
-            .disableAutoConnect() // disable auto-connection
-            .setExtraHeaders({'foo': 'bar'}) // optional
-            .build());
-
-    // ignore: avoid_print
-    socket.onConnect((_) => print('conectado'));
-    // ignore: avoid_print
-    socket.onDisconnect((_) => print('conectado'));
+ 
+  void _initConfig(){  
+    IO.Socket socket = IO.io(
+      "http://10.0.2.2:3000",
+      IO.OptionBuilder()
+        .setTransports(['websocket'])
+        .enableAutoConnect() 
+        .build()
+    );
+ 
+    socket.onConnect((_) {
+      socket.emit('mensaje', 'conectado desde app Flutter');
+      print('connect');
+    });
+ 
+    socket.onDisconnect((_) => print('disconnect'));
   }
 }
