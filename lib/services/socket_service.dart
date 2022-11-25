7887194e64e04ad5c6757992933ffa4f97f4068c
ramonantoniosantoscuevas15 <82469695+ramonantoniosantoscuevas15@users.noreply.github.com>
2022-11-25 10:34:39 -0400

    mejoras

--- lib/services/socket_service.dart
+++ lib/services/socket_service.dart
@@ -1,32 +1,25 @@
+// ignore_for_file: constant_identifier_names
+
 import 'package:flutter/material.dart';
 import 'package:socket_io_client/socket_io_client.dart';
-enum ServerStatus{
-  Online,
-  Offline,
-  Connecting,
-}
- 
+
+enum ServiceStatus { Conneting, Offline, Online }
+
 class SocketService with ChangeNotifier {
-  final ServerStatus _serverStatus = ServerStatus.Connecting;
- 
-  SocketService(){
+  final ServiceStatus _serviceStatus = ServiceStatus.Conneting;
+
+  SocketService() {
     _initConfig();
   }
- 
-  void _initConfig(){  
-    Socket socket = io(
-      "http://10.0.2.2:3000",
+}
+
+void _initConfig() {
+  Socket socket = io(
+      'http://10.0.0.15:3000',
       OptionBuilder()
-        .setTransports(['websocket'])
-        .enableAutoConnect() 
-        .build()
-    );
- 
-    socket.onConnect((_) {
-      socket.emit('mensaje', 'conectado desde app Flutter');
-      print('connect');
-    });
- 
-    socket.onDisconnect((_) => print('disconnect'));
-  }
+          .setTransports(['websocket']) // for Flutter or Dart VM
+          .disableAutoConnect() // disable auto-connection
+          .setExtraHeaders({'foo': 'bar'}) // optional
+          .build());
+  socket.connect();
 }
